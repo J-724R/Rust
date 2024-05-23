c74cb9e67b6c290949e3692d8d45a2acd8df4881
J-724R <jjpernian@gmail.com>
2024-05-23 14:22:14 -0400

    Memory and Allocation

---
+++ Chapter 4/ownership/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let s = "hello world";
+    let mut string = String::from("Hello");
+    string.push_str("World!");
+
+    println!("s variable: {s}");
+    println!("string variable: {s}");
+}
