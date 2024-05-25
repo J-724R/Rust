ee5e631a57784f67fc93c7bfbeeb008dbb4bdd7d
J-724R <jjpernian@gmail.com>
2024-05-25 17:28:21 -0400

    Chapter 4.2 done

--- Chapter 4/reference_and_borrowing/src/main.rs
+++ Chapter 4/reference_and_borrowing/src/main.rs
@@ -2,11 +2,35 @@ fn main() {
     // Refernce, now making the calculate example without borrowing
     println!("Now using references in calculate_length");
 
-    let s1 = String::from("Hello");
+    let mut s1 = String::from("Hello");
     let len = calculate_length(&s1);
     println!("The length of '{s1}' is {}", len);
+
+    // Mutable References
+    println!("\n\nMutable References");
+    change(&mut s1);
+    println!("Mutated s1: {s1}");
+
+    // Dangling References
+    println!("\n\nDangling References");
+    // let reference_to_nothing = dangle();
+    let safe_return = no_dangle();
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world")
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
 }
