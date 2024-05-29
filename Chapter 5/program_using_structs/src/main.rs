8dc630dad91b6d1984a8f128f8c2c4399e8f1255
J-724R <jjpernian@gmail.com>
2024-05-29 18:44:54 -0400

    refactoring with structs

--- Chapter 5/program_using_structs/src/main.rs
+++ Chapter 5/program_using_structs/src/main.rs
@@ -1,3 +1,19 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    println!("Hello, world!");
+    let react1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the reactangle is {} aquare pixels.",
+        area(&react1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
