ad1b6af7f33eff0e39c09e3ccb74472bdb2e245f
J-724R <jjpernian@gmail.com>
2024-06-12 11:42:19 -0400

    Chapter 6 completed

--- Chapter 6/control_flow_with_if_let/src/main.rs
+++ Chapter 6/control_flow_with_if_let/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
+
+    // with the knowledge from previous chapters
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    // With if let syntax
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max)
+    }
+
+    //  Recalling previous example of The coin
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+
+    // Now with if let
+    // let mut count = 0;
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State quarter from {:?}!", state);
+    // } else {
+    //     count += 1;
+    // }
 }
