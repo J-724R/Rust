7e4b61fad944f173b3d8e3c64184b69a1b36caad
J-724R <jjpernian@gmail.com>
2024-06-17 08:57:50 -0400

    Chapter 7.4 completed

--- Chapter 7/Defining_Modules_to_Control_Scope_and_Privacy/backyard/restaurant/src/lib.rs
+++ Chapter 7/Defining_Modules_to_Control_Scope_and_Privacy/backyard/restaurant/src/lib.rs
@@ -72,3 +72,9 @@ mod customer {
 // pub fn eat_at_restaurant2() {
 //     hosting::add_to_waitlist();
 // }
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
