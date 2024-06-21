197bafe6656394567672955e25982310b809f3b6
J-724R <jjpernian@gmail.com>
2024-06-21 13:26:06 -0400

    common way of creating a vector

--- Chapter 8/storing_utf-8_text_with_strings/src/main.rs
+++ Chapter 8/storing_utf-8_text_with_strings/src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     println!("Hello, world!");
 
     let v: Vec<i32> = Vec::new();
+
+    let v = vec![1, 2, 3];
+
+    // The most common way to create a vector is with the generic type Vec<T>
+    // or with the macro vec!
 }
