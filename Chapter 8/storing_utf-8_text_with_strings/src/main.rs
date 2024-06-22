7debdb7bd28c91b9201dce7b997477481aa01737
J-724R <jjpernian@gmail.com>
2024-06-22 12:17:41 -0400

    Reading elements of  Vectors

--- Chapter 8/storing_utf-8_text_with_strings/src/main.rs
+++ Chapter 8/storing_utf-8_text_with_strings/src/main.rs
@@ -7,4 +7,29 @@ fn main() {
 
     // The most common way to create a vector is with the generic type Vec<T>
     // or with the macro vec!
+
+    // Updating a Vector
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    // readin elements of vectors
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let does_not_exist = &v[100];
+    let does_not_exist = v.get(100);
 }
