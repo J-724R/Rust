2cf5d03ca81f6b6c619718934c2322f9f0a98a79
J-724R <jjpernian@gmail.com>
2024-06-25 18:55:08 -0400

    Chapter 8.1 completed

--- Chapter 8/storing_utf-8_text_with_strings/src/main.rs
+++ Chapter 8/storing_utf-8_text_with_strings/src/main.rs
@@ -66,4 +66,9 @@ fn main() {
     }
 
     //Dropping a Vector Drops its elements
+    {
+        let v = vec![1, 2, 3, 4];
+
+        // do stuff with v
+    } // <- v goes out of scope and is freed here
 }
