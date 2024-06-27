9dfc71f3420495011c0e3e62e1946ec5e2d02919
J-724R <jjpernian@gmail.com>
2024-06-27 17:54:15 -0400

    Creating New String

--- Chapter 8/encoded_text_with_strings/src/main.rs
+++ Chapter 8/encoded_text_with_strings/src/main.rs
@@ -2,4 +2,26 @@ fn main() {
     println!("Hello, world!");
 
     // Creatign a New String
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    // strings are UTF-8 encoded, so we can include any properly encoded data in them,
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שלום");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
+
+    // Updating a String
 }
