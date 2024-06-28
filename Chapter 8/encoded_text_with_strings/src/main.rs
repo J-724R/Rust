5083b988852c32058fb7f39f98b0bc545da0692f
J-724R <jjpernian@gmail.com>
2024-06-28 14:12:10 -0400

    Concatenation with the + Operator

--- Chapter 8/encoded_text_with_strings/src/main.rs
+++ Chapter 8/encoded_text_with_strings/src/main.rs
@@ -24,4 +24,18 @@ fn main() {
     let hello = String::from("Hola");
 
     // Updating a String
+
+    // Appending to a String with push_str and push
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+    // Concatenation with the + Operator or the format! Macro
 }
