15ab397fdbc002136b3c61f063b2598b6b4fd02f
J-724R <jjpernian@gmail.com>
2024-07-01 10:28:22 -0400

    Chapter 8.3 done

--- Chapter 8/hash-maps/src/main.rs
+++ Chapter 8/hash-maps/src/main.rs
@@ -1,3 +1,60 @@
+use std::collections::HashMap;
 fn main() {
     println!("Hello, world!");
+
+    // Creating New Hash Map
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // Accessing Values in a Hash Map
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // WE can iterate in a similar manner as in vectors, using a for loop
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    // Hash Maps and Ownership
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+
+    // Updating a Hash Map
+
+    // Overwriting a Value
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
+
+    // Adding a Key and Value Only If a Key Isn’t Present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Red")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
+
+    // Updating a Value Based on the Old Value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
+
+    // Hashing Functions
+
+    // Exercises
 }
