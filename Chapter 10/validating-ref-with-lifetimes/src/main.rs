1b224184ede9fded5110ac33a295a74dbaec865a
J-724R <jjpernian@gmail.com>
2024-07-05 14:23:54 -0400

    Chapter 10.3 lifetime annotation syntax

---
+++ Chapter 10/validating-ref-with-lifetimes/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    // Preventing Dangling References with Lifetimes
+    // Borrow checker
+    // Generic Lifetimes in Functions
+    // Lifetime Annotation Syntax
+}
