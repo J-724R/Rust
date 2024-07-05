14c51d787934092e15de441a7145d26a12729d5b
J-724R <jjpernian@gmail.com>
2024-07-05 16:39:25 -0400

    Chapter 10 completed

--- Chapter 10/validating-ref-with-lifetimes/src/main.rs
+++ Chapter 10/validating-ref-with-lifetimes/src/main.rs
@@ -3,5 +3,7 @@ fn main() {
     // Preventing Dangling References with Lifetimes
     // Borrow checker
     // Generic Lifetimes in Functions
+    // Lifetime Elision
     // Lifetime Annotation Syntax
+    //
 }
