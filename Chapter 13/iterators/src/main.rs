0149e6fb2c23ae9855d171542c13b0211642020c
J-724R <jjpernian@gmail.com>
2024-07-09 18:40:56 -0400

    Chapter 13.3 completed

--- Chapter 13/iterators/src/main.rs
+++ Chapter 13/iterators/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     // Methods that Produce Other Iterators
     let v1: Vec<i32> = vec![1, 2, 3];
 
-    let v2: Vec<_> = v1.map(|x| x + 1).collect();
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
 
     assert_eq!(v2, vec![2, 3, 4]);
 }
