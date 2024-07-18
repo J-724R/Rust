081783ebc3e06a9c1e711e46ce185048998ed6f2
J-724R <jjpernian@gmail.com>
2024-07-18 18:56:30 -0400

    Ignoring unused variables

--- Chapter 18/pattern_syntax/src/main.rs
+++ Chapter 18/pattern_syntax/src/main.rs
@@ -87,4 +87,40 @@ fn main() {
 
     // Destructuring Structs and Tuples
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    //
+    //
+    // Ignoring Values in a Pattern
+
+    // Ignoring an Entire Value with _
+    foo(3, 4);
+
+    // Ignoring Parts of a Value with a Nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {setting_value:?}");
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
+
+    // Ignoring an Unused Variable by Starting Its Name with _
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {y}");
 }
