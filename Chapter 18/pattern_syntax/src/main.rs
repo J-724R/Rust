7deb4a9dd8ad9df4ee34a293f409ae329af33534
J-724R <jjpernian@gmail.com>
2024-07-19 13:10:27 -0400

    Chapter 18 completed

--- Chapter 18/pattern_syntax/src/main.rs
+++ Chapter 18/pattern_syntax/src/main.rs
@@ -119,6 +119,82 @@ fn main() {
     }
 
     // Ignoring an Unused Variable by Starting Its Name with _
+    let _x = 5;
+    let y = 10;
+
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{s:?}");
+
+    // Ignoring Remaining Parts of a Value with ..
+    struct Point3D {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point3D { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point3D { x, .. } => println!("x is {x}"),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    // Extra Conditionals with Match Guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    // @ Bindings
+    enum MessageBindings {
+        Hello { id: i32 },
+    }
+
+    let msg = MessageBindings::Hello { id: 5 };
+
+    match msg {
+        MessageBindings::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        MessageBindings::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        MessageBindings::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
 
 fn foo(_: i32, y: i32) {
