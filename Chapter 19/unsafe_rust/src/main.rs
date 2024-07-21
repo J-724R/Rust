d234e34f56c23122e1b85ae24fb1747adfa8e4b4
J-724R <jjpernian@gmail.com>
2024-07-21 14:33:02 -0400

    Chapter 19.01 completed

--- Chapter 19/unsafe_rust/src/main.rs
+++ Chapter 19/unsafe_rust/src/main.rs
@@ -16,4 +16,51 @@ fn main() {
     }
 
     println!("Creating a Safe Abstraction over Unsafe Code");
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    split_at_mut(&mut vec![1, 2, 3, 4, 5, 6], 3);
+
+    println!("Using extern Functions to Call External Code");
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
+    println!("Calling Rust Functions from Other Languages");
+    #[no_mangle]
+    pub extern "C" fn call_from_c() {
+        println!("Just called a Rust function from C!");
+    }
+
+    println!("Accessing or Modifying a Mutable Static Variable");
+    println!("name is: {HELLO_WORLD}");
+}
+
+static HELLO_WORLD: &str = "Hello, world!";
+
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+use std::slice;
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
 }
