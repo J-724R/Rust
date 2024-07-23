0f7708de1e35373d32a3eb6fbd3a6ce669d1ab9a
J-724R <jjpernian@gmail.com>
2024-07-22 21:09:06 -0400

    chapter 19.2 update

--- Chapter 19/advanced_traits/src/lib.rs
+++ Chapter 19/advanced_traits/src/lib.rs
@@ -27,3 +27,16 @@ impl Iterator for Counter {
         }
     }
 }
+
+use std::ops::Add;
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
--- Chapter 19/advanced_traits/src/main.rs
+++ Chapter 19/advanced_traits/src/main.rs
@@ -1,3 +1,30 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    println!("Default Generic Type Parameters and Operator Overloading");
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    println!("Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name");
 }
