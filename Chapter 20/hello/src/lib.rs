8c785c24b2eba2b40134e2238996f080adba75a7
J-724R <jjpernian@gmail.com>
2024-07-30 20:27:47 -0400

    update

--- Chapter 20/hello/src/lib.rs
+++ Chapter 20/hello/src/lib.rs
@@ -57,11 +57,19 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
 
-            job();
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
         Worker {
@@ -69,6 +77,7 @@ impl Worker {
             thread: Some(thread),
         }
     }
+}
 }
 
 impl Drop for ThreadPool {
@@ -84,3 +93,5 @@ impl Drop for ThreadPool {
         }
     }
 }
+
+// Dropping sender closes the channel, which indicates no more messages will be sent. When that happens, all the calls to recv that the workers do in the infinite loop will return an error. In Listing 20-24, we change the Worker loop to gracefully exit the loop in that case, which means the threads will finish when the ThreadPool drop implementation calls join on them.
